d469ffc0beb1a5543d1352714611d6b0e3432d0e
Pawan Kumar <pawan221b@gmail.com>
2018-11-07 17:21:08 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,6 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    fetchQuestions();
   }
 
   Future<void> fetchQuestions() async {
@@ -41,7 +40,6 @@ class _HomePageState extends State<HomePage> {
     c = Colors.black;
     quiz = Quiz.fromJson(decRes);
     results = quiz.results;
-    setState(() {});
   }
 
   @override
@@ -51,79 +49,117 @@ class _HomePageState extends State<HomePage> {
         title: Text("Quiz App"),
         elevation: 0.0,
       ),
-      body: quiz == null
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : RefreshIndicator(
-              onRefresh: fetchQuestions,
-              child: ListView.builder(
-                itemCount: results.length,
-                itemBuilder: (context, index) => Card(
-                      color: Colors.white,
-                      elevation: 0.0,
-                      child: ExpansionTile(
-                        title: Padding(
-                          padding: const EdgeInsets.all(18.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Text(
-                                results[index].question,
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              FittedBox(
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: <Widget>[
-                                    FilterChip(
-                                      backgroundColor: Color(0xff000000 +
-                                              random.nextInt(0x00ffffff))
-                                          .withOpacity(0.4),
-                                      label: Text(results[index].category),
-                                      onSelected: (b) {},
-                                    ),
-                                    SizedBox(
-                                      width: 10.0,
-                                    ),
-                                    FilterChip(
-                                      backgroundColor: Color(0xff000000 +
-                                              random.nextInt(0xff000000))
-                                          .withOpacity(0.4),
-                                      label: Text(results[index].difficulty),
-                                      onSelected: (b) {},
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.grey[100],
-                          child: Text(
-                              results[index].type.startsWith("m") ? "M" : "B"),
-                        ),
-                        children: results[index].allAnswers.map((m) {
-                          return AnswerWidget(results, index, m);
-                        }).toList(),
+      body: RefreshIndicator(
+        onRefresh: fetchQuestions,
+        child: FutureBuilder(
+            future: fetchQuestions(),
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Text('Press button to start.');
+                case ConnectionState.active:
+                case ConnectionState.waiting:
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasError) return errorData(snapshot);
+                  return questionList();
+              }
+              return null;
+            }),
+      ),
+    );
+  }
+
+  Padding errorData(AsyncSnapshot snapshot) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Error: ${snapshot.error}',
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          RaisedButton(
+            child: Text("Try Again"),
+            onPressed: () {
+              fetchQuestions();
+              setState(() {});
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  ListView questionList() {
+    return ListView.builder(
+      itemCount: results.length,
+      itemBuilder: (context, index) => Card(
+            color: Colors.white,
+            elevation: 0.0,
+            child: ExpansionTile(
+              title: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(
+                      results[index].question,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    FittedBox(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          FilterChip(
+                            backgroundColor:
+                                Color(0xff000000 + random.nextInt(0x00ffffff))
+                                    .withOpacity(0.4),
+                            label: Text(results[index].category),
+                            onSelected: (b) {},
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          FilterChip(
+                            backgroundColor:
+                                Color(0xff000000 + random.nextInt(0xff000000))
+                                    .withOpacity(0.4),
+                            label: Text(results[index].difficulty),
+                            onSelected: (b) {},
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
+              leading: CircleAvatar(
+                backgroundColor: Colors.grey[100],
+                child: Text(results[index].type.startsWith("m") ? "M" : "B"),
+              ),
+              children: results[index].allAnswers.map((m) {
+                return AnswerWidget(results, index, m);
+              }).toList(),
             ),
+          ),
     );
   }
 }
 
 class AnswerWidget extends StatefulWidget {
-  List<Results> results;
-  int index;
-  String m;
+  final List<Results> results;
+  final int index;
+  final String m;
 
   AnswerWidget(this.results, this.index, this.m);
 
